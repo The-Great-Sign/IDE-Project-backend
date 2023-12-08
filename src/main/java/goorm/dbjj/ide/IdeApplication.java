24d28866e9c957feb47aa11e0b3df7ff336f4fbc
sbslc2000 <sbslc2000@gmail.com>
2023-12-08 20:39:17 +0900

    first commit

---
+++ src/main/java/goorm/dbjj/ide/IdeApplication.java
@@ -0,0 +1,13 @@
+package goorm.dbjj.ide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/goorm/dbjj/ide/IdeApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.dbjj.ide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
