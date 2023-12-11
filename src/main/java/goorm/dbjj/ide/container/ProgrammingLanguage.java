d82955fb6db939580f35eaf9cd2727bb955c74d0
sbslc2000 <sbslc2000@gmail.com>
2023-12-11 16:32:11 +0900

    docs: IDE-5 ProgrammingLanguage 주석 추가

--- src/main/java/goorm/dbjj/ide/container/ProgrammingLanguage.java
+++ src/main/java/goorm/dbjj/ide/container/ProgrammingLanguage.java
@@ -4,12 +4,21 @@ import lombok.Getter;
 
 import java.util.List;
 
-
+/**
+ * 애플리케이션 내에서 다루는 프로그래밍 언어를 ENUM으로 정의한 클래스입니다.
+ */
 @Getter
 public enum ProgrammingLanguage {
     PYTHON("python:3", List.of("python","-m","http.server","8000"));
 
+    /**
+     * Docker Hub에서 사용하는 이미지 이름
+     */
     private final String image;
+
+    /**
+     * 컨테이너가 실행될 때 실행할 명령어
+     */
     private final List<String> command;
 
     ProgrammingLanguage(String image, List<String> command) {
