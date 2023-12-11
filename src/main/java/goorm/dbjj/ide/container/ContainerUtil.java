5d2f8d1798669d0452b94fd8542f97b7352df62a
sbslc2000 <sbslc2000@gmail.com>
2023-12-11 11:38:24 +0900

    feat: IDE-5 ContaierUtil.createProjectImage 주석 추가

--- src/main/java/goorm/dbjj/ide/container/ContainerUtil.java
+++ src/main/java/goorm/dbjj/ide/container/ContainerUtil.java
@@ -22,6 +22,15 @@ public class ContainerUtil {
     public void executeCommand(String projectId, String command) {
     }
 
+    /**
+     * 프로젝트 컨테이너 이미지를 생성하는 메서드
+     *
+     * 프로젝트의 생성과 동시에 컨테이너의 이미지를 생성합니다.
+     * 수행 이후 Project.taskDefinition에 이미지의 식별자가 저장됩니다.
+     *
+     * @param project - 컨테이너 이미지를 생성할 프로젝트
+     * @param accessPointId - EFS에 AccessPoint를 생성하고 그 ID를 파라미터로 전달받아야합니다.
+     */
     public void createProjectImage(Project project, String accessPointId) {
         log.trace("ContainerUtil.createProjectImage called");
 
