ab023ca7791be3ac9dd4148bd599e1efba61056c
sbslc2000 <sbslc2000@gmail.com>
2023-12-11 11:30:23 +0900

    feat: IDE-5 ContaierUtil 컨테이너 실행 메서드 개발

--- src/main/java/goorm/dbjj/ide/container/ContainerUtil.java
+++ src/main/java/goorm/dbjj/ide/container/ContainerUtil.java
@@ -14,6 +14,7 @@ public class ContainerUtil {
 
     private final EcsClient ecsClient;
     private final TaskDefinitionHelper taskDefinitionHelper;
+    private final RunTaskRequestHelper runTaskRequestHelper;
 
     /**
      * Todo: 컨테이너에 명령어를 수행시키는 메서드
@@ -35,8 +36,22 @@ public class ContainerUtil {
         project.setTaskDefinition(taskDefResponse.taskDefinition().taskDefinitionArn());
     }
 
+    /**
+     * 프로젝트의 컨테이너를 실행시킵니다.
+     *
+     * 실행 설정 정보 생성 -> 실행 -> 컨테이너 ID 반환
+     *
+     * @param project
+     * @return 컨테이너 식별자 - 추후 저장 필요
+     */
+    public String runProjectContainer(Project project) {
+        log.trace("ContainerUtil.runProjectContainer called");
 
-    public void runProjectContainer(Project project) {
+        RunTaskRequest runTaskRequest = runTaskRequestHelper.createRunTaskRequest(project.getTaskDefinition());
+        RunTaskResponse runTaskResponse = ecsClient.runTask(runTaskRequest);
+
+        log.debug("runTaskResponse: {}", runTaskResponse);
+        return runTaskResponse.tasks().get(0).taskArn();
     }
 
     public void stopProjectContainer() {
--- src/test/java/goorm/dbjj/ide/container/ContainerUtilTest.java
+++ src/test/java/goorm/dbjj/ide/container/ContainerUtilTest.java
@@ -1,11 +1,15 @@
 package goorm.dbjj.ide.container;
+
 import goorm.dbjj.ide.domain.Project;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import software.amazon.awssdk.services.ecs.EcsClient;
 import software.amazon.awssdk.services.ecs.model.DeregisterTaskDefinitionRequest;
+import software.amazon.awssdk.services.ecs.model.StopTaskRequest;
 import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
@@ -30,7 +34,7 @@ class ContainerUtilTest {
                 null
         );
 
-        containerUtil.createProjectImage(project,"fsap-04fbb958d168856f3");
+        containerUtil.createProjectImage(project, "fsap-04fbb958d168856f3");
 
         assertThat(project.getTaskDefinition()).isNotNull();
 
@@ -38,6 +42,39 @@ class ContainerUtilTest {
         ecsClient.deregisterTaskDefinition(DeregisterTaskDefinitionRequest.builder()
                 .taskDefinition(project.getTaskDefinition())
                 .build());
+    }
 
+    /**
+     * 특정 프로젝트의 컨테이너를 수행시키는 테스트입니다.
+     * 에러 발생 시 팀장에게 빠른 전달 부탁합니다. (과금과 관계 있음)
+     */
+    @Test
+    void runProjectContainer() {
+        Project project = new Project(
+                UUID.randomUUID().toString(),
+                "test",
+                "test",
+                null, // taskDefinition
+                ProgrammingLanguage.PYTHON,
+                "test",
+                null
+        );
+
+        containerUtil.createProjectImage(project, "fsap-04fbb958d168856f3");
+
+        Assertions.assertDoesNotThrow(() -> {
+            String taskArn = containerUtil.runProjectContainer(project);
+
+            StopTaskRequest stopTaskRequest = StopTaskRequest.builder()
+                    .cluster("IDE_CONTAINER")
+                    .task(taskArn)
+                    .build();
+
+            ecsClient.stopTask(stopTaskRequest);
+
+            ecsClient.deregisterTaskDefinition(DeregisterTaskDefinitionRequest.builder()
+                    .taskDefinition(project.getTaskDefinition())
+                    .build());
+        });
     }
 }
