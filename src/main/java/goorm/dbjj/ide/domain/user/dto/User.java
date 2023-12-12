7c6ba8be93687a286bfb9e357c4666aa15e02a23
HJunng <guswjd1gh@gmail.com>
2023-12-12 13:42:39 +0900

    feat: IDE-22 User Entity 생성 (+ enum 타입 정의)

---
+++ src/main/java/goorm/dbjj/ide/domain/user/dto/User.java
@@ -0,0 +1,59 @@
+package goorm.dbjj.ide.domain.user.dto;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 상속받는 자식 클래스까지만 인스턴스 생성 가능.
+@Builder
+@Table(name = "Users") // 예약어 충돌
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email; // 이메일
+    private String nickname; // 닉네임
+    private String imageUrl; // 프로필 이미지
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType; // GOOGLE
+
+    private String socialId; // 소셜로그인한 id
+    private String refreshToken; // 리프레시 토큰
+
+    @ColumnDefault("NOW()")
+    private LocalDateTime createdAt;
+
+    @ColumnDefault("NOW()")
+    private LocalDateTime updatedAt;
+
+    // 유저 권한 설정 메서드
+    public void authorizedUser(){
+        this.role = Role.USER;
+    }
+
+    // 유저 필드 업데이트
+    public void updateNickname(String updateNickname){
+        this.nickname = updateNickname;
+        updateTime();
+    }
+
+    public void updateRefreshToken(String updateRefreshToken){
+        this.refreshToken = updateRefreshToken;
+        updateTime();
+    }
+
+    public void updateTime(){
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
