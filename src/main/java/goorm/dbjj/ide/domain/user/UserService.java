78c927da7a3f9d8d4f6e2f3e4f965bef0faeedc6
HJunng <guswjd1gh@gmail.com>
2023-12-12 16:44:57 +0900

    feat: IDE-22 유저 정보 조회시 안될 경우 BaseException 처리

--- src/main/java/goorm/dbjj/ide/domain/user/UserService.java
+++ src/main/java/goorm/dbjj/ide/domain/user/UserService.java
@@ -1,7 +1,9 @@
 package goorm.dbjj.ide.domain.user;
 
+import goorm.dbjj.ide.api.exception.BaseException;
 import goorm.dbjj.ide.domain.user.dto.User;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.engine.jdbc.batch.internal.BasicBatchKey;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,7 +14,7 @@ public class UserService {
     private final UserRepository userRepository;
 
     public User getUser(String email){
-        Optional<User> user = userRepository.findByEmail(email);
-        return user.orElse(null);
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new BaseException(email+"에 해당하는 정보가 없습니다."));
     }
 }
