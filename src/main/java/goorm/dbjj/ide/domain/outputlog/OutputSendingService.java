e1cc8b2d857965263c6bcb1e2fe3d91d78e1cdd6
sbslc2000 <sbslc2000@gmail.com>
2023-12-13 16:03:27 +0900

    feat: IDE-31-OutputSendingService 주석 추가

--- src/main/java/goorm/dbjj/ide/domain/outputlog/OutputSendingService.java
+++ src/main/java/goorm/dbjj/ide/domain/outputlog/OutputSendingService.java
@@ -7,5 +7,10 @@ import goorm.dbjj.ide.domain.outputlog.dto.request.LogEntry;
  * 결과는 lambda로부터 받아서 웹소켓으로 전송됩니다.
  */
 public interface OutputSendingService {
+
+    /**
+     * 사용자에게 수행한 결과를 제공합니다.
+     * @param logEntry
+     */
     void sendTo(LogEntry logEntry);
 }
--- src/main/java/goorm/dbjj/ide/domain/outputlog/dto/response/ExecutionOutputDto.java
+++ src/main/java/goorm/dbjj/ide/domain/outputlog/dto/response/ExecutionOutputDto.java
@@ -8,10 +8,15 @@ import lombok.ToString;
  * 사용자가 수행한 결과를 제공하는 클래스입니다.
  */
 @ToString
-@AllArgsConstructor
 @Getter
 public class ExecutionOutputDto {
     private boolean success;
     private String path;
     private String content;
+
+    public ExecutionOutputDto(boolean success, String content, String path) {
+        this.success = success;
+        this.content = content;
+        this.path = path;
+    }
 }
