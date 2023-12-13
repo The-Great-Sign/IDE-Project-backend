7eef187cd0e0dd0e4be4471129e69c1248e23c88
HJunng <guswjd1gh@gmail.com>
2023-12-13 17:10:55 +0900

    feat: IDE-27 주석 처리 해제

--- src/main/java/goorm/dbjj/ide/domain/user/UserRepository.java
+++ src/main/java/goorm/dbjj/ide/domain/user/UserRepository.java
@@ -1,10 +1,13 @@
-//package goorm.dbjj.ide.domain.user;
-//
-//import goorm.dbjj.ide.domain.user.dto.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.Optional;
-//
-//public interface UserRepository extends JpaRepository<User,Long> {
-//    Optional<User> findByEmail(String email);
-//}
+package goorm.dbjj.ide.domain.user;
+
+import goorm.dbjj.ide.domain.user.dto.SocialType;
+import goorm.dbjj.ide.domain.user.dto.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
+}
