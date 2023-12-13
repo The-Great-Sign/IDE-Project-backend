ae1a72d7d505a9ec3ba725ce2a7f5b10636233df
HJunng <guswjd1gh@gmail.com>
2023-12-13 20:17:59 +0900

    feat: IDE-27 리다이렉트 url를 yml로 이동

--- src/main/java/goorm/dbjj/ide/auth/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/goorm/dbjj/ide/auth/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -6,6 +6,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -18,7 +22,9 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
-    private final UserRepository userRepository;
+
+    @Value("${app.redirect-url}")
+    private String redirectUrl;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
@@ -41,10 +47,6 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
      * 지금은 임시 리디렉션 걸어놈. 회원이면 들어갈 수 있는 페이지로 이동.
       */
     private void loginSuccess(HttpServletResponse response, DefaultOAuth2User oAuth2User) throws IOException {
-
-        // 리디렉션할 URL 설정
-        String redirectUrl = "http://localhost:8080/user/main";
-
         // 임시로 리디렉션 추가. 회원이면 들어갈 수 있는 페이지로 이동.
         response.setStatus(HttpServletResponse.SC_FOUND); // 302 상태코드
         response.setHeader("Location", redirectUrl);
