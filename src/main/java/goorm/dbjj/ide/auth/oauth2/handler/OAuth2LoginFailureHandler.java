9d73e8bb6e1e26df3f3bb38b0c10f7a7fb47b224
HJunng <guswjd1gh@gmail.com>
2023-12-14 01:07:39 +0900

    feat: IDE-27 log.debug 단위로 변경하고, 주석을 확실히 작성.

--- src/main/java/goorm/dbjj/ide/auth/oauth2/handler/OAuth2LoginFailureHandler.java
+++ src/main/java/goorm/dbjj/ide/auth/oauth2/handler/OAuth2LoginFailureHandler.java
@@ -1,6 +1,5 @@
 package goorm.dbjj.ide.auth.oauth2.handler;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -14,10 +13,14 @@ import java.io.IOException;
 @Component
 public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
 
+    /**
+     * 인증 절차 자체에서 실패하였을 때 처리.
+     * ex) OAuth2 제공자와의 통신 중 문제, 리디렉션 불일치 등.
+     */
     @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
         response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         response.getWriter().write("소셜로그인 실패! 로그를 확인해주세요.");
-        log.info("소셜 로그인에 실패했습니다. 에러메세지: {}",exception.getMessage());
+        log.debug("소셜 로그인에 실패했습니다. 에러메세지: {}",exception.getMessage());
     }
 }
