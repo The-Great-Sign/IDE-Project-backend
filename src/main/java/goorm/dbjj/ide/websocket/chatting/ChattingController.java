5315a8c0d15f9d8d6224b3130127af08c98f820b
leebongseung <yui5227@gmail.com>
2023-12-13 22:09:00 +0900

    feat : IDE-33 ChatsController 개선

--- src/main/java/goorm/dbjj/ide/websocket/chatting/ChattingController.java
+++ src/main/java/goorm/dbjj/ide/websocket/chatting/ChattingController.java
@@ -8,6 +8,7 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
@@ -20,23 +21,38 @@ public class ChattingController {
     private final ChatsService chatsService;
     private final SimpMessagingTemplate template;
 
+    /**
+     * 채팅방 subscribe 시 입장 알림 및 웹 소켓 세션 등록하기
+     * */
     @SubscribeMapping("/project/{projectId}/chat")
     public void enter(
+            SimpMessageHeaderAccessor headerAccessor,
             @DestinationVariable("projectId") Long projectId
     ){
         log.trace("ChattingController.enter execute");
-        //todo: 유저 매핑하는 것 해야한다.
+
+        // todo : jwt? 시큐리티? 등을 이용해서 유저정보 가져오기
         Long userId = 1L;
-        template.convertAndSend("/topic/project/"+projectId+"/chat", chatsService.enter(userId));
+
+        ChattingResponseDto enterMessage = chatsService.enter(headerAccessor, projectId, userId);
+        template.convertAndSend("/topic/project/"+projectId+"/chat",enterMessage);
     }
 
+    /**
+     * 채팅방 subscribe 시 입장 알림
+     * */
     @MessageMapping("/project/{projectId}/chat-create")
     @SendTo("/topic/project/{projectId}/chat")
     public ChattingResponseDto talk(
+            SimpMessageHeaderAccessor headerAccessor,
             @DestinationVariable("projectId") Long projectId,
             @Payload ChattingContentRequestDto chatsDto
     ) {
         log.trace("ChattingController.chatting execute");
-        return chatsService.talk(chatsDto);
+
+        // todo : jwt? 시큐리티? 등을 이용해서 유저정보 가져오기
+        Long userId = 1L;
+
+        return chatsService.talk(chatsDto, userId);
     }
 }
