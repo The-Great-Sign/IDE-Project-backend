8d99369517c81dc0bf3ed0c116696e6c0eb9ed4f
sbslc2000 <sbslc2000@gmail.com>
2023-12-13 22:02:31 +0900

    feat: IDE-35-ProjectRepository 생성

--- src/main/java/goorm/dbjj/ide/domain/project/ProjectRepository.java
+++ src/main/java/goorm/dbjj/ide/domain/project/ProjectRepository.java
@@ -1,5 +1,6 @@
 package goorm.dbjj.ide.domain.project;
 
+import goorm.dbjj.ide.domain.project.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectRepository extends JpaRepository<Project, String> {
