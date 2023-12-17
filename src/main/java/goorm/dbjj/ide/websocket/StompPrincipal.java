290d03a36c5fe27379f0bd1de90df4bc8317d503
leebongseung <yui5227@gmail.com>
2023-12-18 03:39:13 +0900

    feat : IDE-58 사용자의 정보를 저장하는 로직 추가(/user)로 보내기위해 필요

---
+++ src/main/java/goorm/dbjj/ide/websocket/StompPrincipal.java
@@ -0,0 +1,16 @@
+package goorm.dbjj.ide.websocket;
+
+import java.security.Principal;
+
+public class StompPrincipal implements Principal {
+    private String name;
+
+    public StompPrincipal(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
