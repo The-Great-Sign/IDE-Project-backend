0ad5a66d61113c2c37adee3acaa5ae57b2dc170b
sbslc2000 <sbslc2000@gmail.com>
2023-12-17 23:00:39 +0900

    feat: IDE-67-예외 케이스 처리 및 주석 추가

--- src/main/java/goorm/dbjj/ide/util/filewatcher/FileWatcher.java
+++ src/main/java/goorm/dbjj/ide/util/filewatcher/FileWatcher.java
@@ -8,8 +8,15 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 
+/**
+ * FileWatcher
+ *
+ * FileWatcher은 EFS 스토리지의 디렉터리를 감시하고,
+ * 디렉터리에 변경사항이 생길 때 이를 사용자에게 알리는 역할을 합니다.
+ * 이를 통해 File의 CRUD 로직에서 생기는 '사용자에게 알리는 책임'을 분리해낼 수 있습니다
+ */
 @Slf4j
-@Component("myFileWatcher")
+@Component("myFileWatcher") //SslAutoConfiguration에 이미 FileWatcher가 등록되어 있어서 이름을 변경해줘야 합니다.
 public class FileWatcher {
 
     public void watch(String dir) throws Exception {
@@ -33,6 +40,10 @@ public class FileWatcher {
                 System.out.println("delete directory = " + directory);
             }
 
+            /**
+             * 현재 swap 파일의 생성 역시 감지되는 이슈가 있습니다.
+             * @param file The file created (ignored)
+             */
             @Override
             public void onFileCreate(File file) {
                 sendToUser(extractProjectId(file.getPath()));
@@ -42,13 +53,15 @@ public class FileWatcher {
             public void onFileDelete(File file) {
                 sendToUser(extractProjectId(file.getPath()));
             }
+
         });
 
+        // 1초마다 변경사항을 감지합니다.
         FileAlterationMonitor monitor = new FileAlterationMonitor(1000);
         monitor.addObserver(observer);
 
         monitor.start();
-        log.info("File Watcher Start, dir : {}", dir);
+        log.info("FileWatcher Start, dir : {}", dir);
     }
 
     /**
@@ -57,11 +70,26 @@ public class FileWatcher {
      * @param projectId
      */
     private void sendToUser(String projectId) {
-        System.out.println("sendToUser = " + projectId);
+        if(projectId != null) {
+            //do nothing
+        } else {
+            //WS로 변경사항을 전송합니다.
+            System.out.println("sendToUser = " + projectId);
+        }
     }
 
+    /**
+     * EFS 스토리지의 디렉터리 경로에서 프로젝트 아이디를 추출합니다.
+     * 로컬에서는 작동하지 않습니다.
+     * @param path
+     * @return
+     */
     private String extractProjectId(String path) {
-        String[] split = path.split("/");
-        return split[5];
+        try { // 로컬 개발환경에서 NPE 발생을 막아주기 위해 try로 감싸줍니다.
+            String[] split = path.split("/");
+            return split[5];
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
--- src/main/java/goorm/dbjj/ide/util/filewatcher/FileWatcherRunner.java
+++ src/main/java/goorm/dbjj/ide/util/filewatcher/FileWatcherRunner.java
@@ -6,6 +6,12 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+/**
+ * FileWatcherRunner
+ *
+ * FileWatcher를 애플리케이션 로딩이 끝난 시점에 수행시키는 역할을 합니다.
+ * 파일을 감지하는 위치는 aws.efs.mountPoint에서 정의합니다.
+ */
 @Component("myFileWatcherRunner")
 @RequiredArgsConstructor
 public class FileWatcherRunner implements ApplicationRunner {
