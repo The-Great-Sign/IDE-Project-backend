b9e92f25b8666fd86d1f40808c256e24f36b3a09
leebongseung <yui5227@gmail.com>
2023-12-19 18:27:55 +0900

    feat : IDE-91 컨테이너 로딩 컨테이너 개발 및 주석 변경

--- src/main/java/goorm/dbjj/ide/websocket/containerloading/ContainerLoadingController.java
+++ src/main/java/goorm/dbjj/ide/websocket/containerloading/ContainerLoadingController.java
@@ -1,4 +1,4 @@
-package goorm.dbjj.ide.websocket.filedirectory;
+package goorm.dbjj.ide.websocket.containerloading;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,18 +8,18 @@ import org.springframework.stereotype.Controller;
 @Slf4j
 @Controller
 @RequiredArgsConstructor
-public class WebSocketFileDirectoryController {
+public class ContainerLoadingController {
 
-    private final SimpMessagingTemplate simpMessagingTemplate;
+    private final SimpMessagingTemplate template;
 
     /**
      * 파일 디렉터리 관련 모든 이벤트를 클라이언트에 전송하는 로직
      * @param projectId, object
-    * */
-    public void broadcastFileAndDirectoryDetails(
-        String projectId,
-        Object object
+     * */
+    public void broadcastContainerLoading(
+            String projectId,
+            Object object
     ){
-        simpMessagingTemplate.convertAndSend("/topic/project/" + projectId + "/file", object);
+        template.convertAndSend("/topic/project/"+projectId+"/container-loading",object);
     }
 }
