54a4c7d0ce8dc014c2be4fb6b925b4f9d650bc2d
CharminGyu <chamingyu726@naver.com>
2023-12-21 14:30:20 +0900

    Refactor : IDE-93 DTO NotNull추가

--- src/main/java/goorm/dbjj/ide/model/dto/FileDeleteRequestDto.java
+++ src/main/java/goorm/dbjj/ide/model/dto/FileDeleteRequestDto.java
@@ -1,5 +1,6 @@
 package goorm.dbjj.ide.model.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Data
@@ -8,6 +9,8 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class FileDeleteRequestDto {
+    @NotNull(message = "projectId에는 null 값이 있으면 안됩니다.")
     private String projectId;
+    @NotNull(message = "경로에 비어있는 값이 있으면 안됩니다.")
     private String path;
 }
