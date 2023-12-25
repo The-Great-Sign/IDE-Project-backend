ab5a4f25290cdb5d7f367665a7d8d9e7fbcf7d1a
sbslc2000 <sbslc2000@gmail.com>
2023-12-25 17:15:46 +0900

    feat: IDE-130-Validated 애노테이션 exception catch

--- src/main/java/goorm/dbjj/ide/api/GlobalRestControllerAdvice.java
+++ src/main/java/goorm/dbjj/ide/api/GlobalRestControllerAdvice.java
@@ -3,6 +3,7 @@ package goorm.dbjj.ide.api;
 import goorm.dbjj.ide.api.exception.BaseException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -25,6 +26,13 @@ public class GlobalRestControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BaseException.class)
     public ApiResponse<String> handleBaseException(BaseException e) {
+        log.debug("Exception catched in RestControllerAdvice : {}",e.getMessage());
+        return ApiResponse.fail(e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiResponse<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
         log.debug("Exception catched in RestControllerAdvice : {}",e.getMessage());
         return ApiResponse.fail(e.getMessage());
     }
