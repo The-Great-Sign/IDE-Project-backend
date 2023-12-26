7af1ce6b643af632d1aa1aa130da075cb8f5a051
sbslc2000 <sbslc2000@gmail.com>
2023-12-26 12:01:43 +0900

    feat: IDE-139-pwd 명령어 수행시 /app 출력되는 문제 제거

--- src/main/java/goorm/dbjj/ide/lambdahandler/executionoutput/OutputSendingServiceImpl.java
+++ src/main/java/goorm/dbjj/ide/lambdahandler/executionoutput/OutputSendingServiceImpl.java
@@ -47,12 +47,25 @@ public class OutputSendingServiceImpl implements OutputSendingService {
         try {
             return switch (splitedMessage.length) {
                 case 1 -> new ExecutionOutputDto(true, "", extractor.extract(splitedMessage[0]));
-                case 2 -> new ExecutionOutputDto(true, splitedMessage[0], extractor.extract(splitedMessage[1]));
+                case 2 -> new ExecutionOutputDto(true, contentWrapper(splitedMessage[0]), extractor.extract(splitedMessage[1]));
                 default -> new ExecutionOutputDto(false, "알 수 없는 오류가 발생했습니다.", "/");
             };
         } catch (Exception e) {
             log.debug("메시지 파싱 중 오류가 발생했습니다. : {}", e.getMessage());
             return new ExecutionOutputDto(false, "알 수 없는 오류가 발생했습니다.", "/");
+        }
+    }
+
+    private String contentWrapper(String content) {
+        if(content.startsWith("/app")) {
+            String newContent = content.substring(4);
+            if(newContent.startsWith("/")) {
+                return newContent;
+            } else {
+                return "/" + newContent;
+            }
+        } else {
+            return content;
         }
     }
 }
